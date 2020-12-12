81cc987efa3e9d96cf9eb0e98e75f4a50700095c
Eugene Hong <doluseventualis99@gmail.com>
2020-12-12 23:28:53 +0900

    servlets

---
+++ Servlet/src/ServletConfigEx.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns= {"/InitParam"},
+		initParams= {@WebInitParam(name="name", value="ȫ�浿"),
+					 @WebInitParam(name="id", value="zzz1234")})
+
+public class ServletConfigEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public ServletConfigEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
